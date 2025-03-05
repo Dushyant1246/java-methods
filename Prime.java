6af263254060f05b41a48847568b00bdb9af9d9b
Dushyant1246 <dushyant.dhmn808@gmail.com>
2025-03-05 22:55:46 +0530

    [Dushyant] ADD: perform number-based checks like prime, neon, spy, buzz

---
+++ Prime.java
@@ -0,0 +1,61 @@
+// class Prime to perform number-based checks like prime, neon, spy, buzz, and automorphic.
+import java.util.Scanner;
+
+public class Prime {
+    // Method to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    // Method to check if a number is a Neon number (sum of digits of square == number)
+    public static boolean isNeon(int num) {
+        int square = num * num;
+        int sum = 0;
+        while (square > 0) {
+            sum += square % 10;
+            square /= 10;
+        }
+        return sum == num;
+    }
+
+    // Method to check if a number is a Spy number (sum of digits == product of digits)
+    public static boolean isSpy(int num) {
+        int sum = 0, product = 1;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit;
+            product *= digit;
+            num /= 10;
+        }
+        return sum == product;
+    }
+
+    // Method to check if a number is an Automorphic number (square ends with the number itself)
+    public static boolean isAutomorphic(int num) {
+        int square = num * num;
+        return String.valueOf(square).endsWith(String.valueOf(num));
+    }
+
+    // Method to check if a number is a Buzz number (divisible by 7 or ends with 7)
+    public static boolean isBuzz(int num) {
+        return num % 7 == 0 || num % 10 == 7;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        System.out.println("Is Prime? " + isPrime(num));
+        System.out.println("Is Neon? " + isNeon(num));
+        System.out.println("Is Spy? " + isSpy(num));
+        System.out.println("Is Automorphic? " + isAutomorphic(num));
+        System.out.println("Is Buzz? " + isBuzz(num));
+
+        scanner.close();
+    }
+}
